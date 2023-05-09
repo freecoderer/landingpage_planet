252ca4b8bf284f902ddbce89b50f2d281e6d79af
freecoderer <110393847+freecoderer@users.noreply.github.com>
2023-05-10 01:15:17 +0900

    feat:structured code v0.1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { Navbar, Nav, Container, Button, Form, Col, Row } from 'react-bootstrap';
+import { Nav, Container, Button, Form, Alert, Col, Row } from 'react-bootstrap';
 import './App.css';
-import React,{useState} from 'react';
+import React,{useState, useRef} from 'react';
 import { Wheel } from 'react-custom-roulette';
-import planet from './planet.png'
+import emailjs from '@emailjs/browser';
+import { FullPage, Slide } from 'react-full-page';
 
 const data = [
   { option: '삼복', style: { backgroundColor: '#769ECB' , textColor: 'black' } },
@@ -21,107 +22,123 @@ function App() {
     setMustSpin(true);
   };
 
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          alert('메일 전송 완료!');
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div className='full'>
-      <Navbar  className='navclass' expand="lg">
-      <Container>
-          <Navbar.Brand href="#into"><img className ='img' src={planet} alt='asdf'></img>&nbsp;&nbsp;&nbsp;냠냠플래닛</Navbar.Brand>
-        </Container>
-      </Navbar>
-      <section id="intro">
-        <Container className='firstclass'>
-            <Row>
-              <Col>
-                <h1>배고픈 점심시간... 뭐먹을지 모르겠다면?</h1>
-              </Col>
-              <Col>
-                <img src='assets/img/q.png' alt="logo"/> 
-              </Col>
-            </Row>
-            <Container className='blank'>
-            </Container>
-            <Row>
-              <Col>
-              </Col>
-              <Col className='ButtonClass'>
-                <Button className="btn float-end btnclass"><Nav.Link href="#contact">테스터 신청</Nav.Link></Button>
-              </Col>
-            </Row>
-          </Container>
-      </section>
-      <section id="into">
-        <Container className='text-center'>
-          <h2>냠냠플래닛 소개</h2>
-          <p className='parah'>냠냠플래닛은 선택장애를 위한 점심시간 쿠폰 제공 서비스를 목표로 하여 만들어진 서비스 입니다</p>
-          <div className='wheeldiv'>
-            <Wheel
-              mustStartSpinning={mustSpin}
-              prizeNumber={prizeNumber}
-              data={data}
-              onStopSpinning={() => {
-                setMustSpin(false);}}/>
-          </div>
-          <Button size="lg" className="btn float-center submitclass" onClick={handleSpinClick} type="submit"> 돌리기!</Button>
-        </Container>
-      </section>
-      <section id="main">
-        <Container>
-          <h2>냠냠플래닛 목표</h2>
-          <p className='parah'>이러이러한 목표를 가지고 있습니다</p>
-        </Container>
-
-        <Container>
-          <h2>냠냠플래닛 사용자 불편사항 이걸로 만들게된 계기</h2>
-          <p className='parah'>이러이러한 목표를 가지고 있습니다</p>
-        </Container>
-      </section>
-      <section id="plan">
-        <Container>
-          <h2>냠냠플래닛 계획</h2>
-          <p>국제관 앞에서 언제 어디서 ~~한 룰렛을 돌릴 예정입니다</p>
-        </Container>
-        <Container>
-          <h2>냠냠플래닛 베니핏</h2>
-          <p>이러이러한 베니핏이 있을 예정입니다</p>
-        </Container>
-      </section>
-      <section id="contact">
-        <Container>
-          <h2>베타테스터 신청</h2>
-          <Form>
-            <Form.Group controlId="formBasicName">
-              <Form.Label>이름</Form.Label>
-              <Form.Control type="text" placeholder="이름 입력" required />
-            </Form.Group>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>학과</Form.Label>
-              <Form.Control type="text" placeholder="학과 입력" required />
-            </Form.Group>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>학번</Form.Label>
-              <Form.Control type="number" placeholder="학번 입력" required />
-            </Form.Group>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>이메일</Form.Label>
-              <Form.Control type="email" placeholder="이메일 입력" required />
-            </Form.Group>
-              <Container className="text-center">
-                <Button size="lg" className="btn float-center submitclass" type="submit">
-                  신청하기
-                </Button>
+      <FullPage controls controlsProps={{ className: "slide-navigation" }}>
+        <Slide>
+          <section id="intro">
+            <Container className='firstclass'>
+              <Row>
+                <Col>
+                  <h1>배고픈 점심시간... 뭐먹을지 모르겠다면?</h1>
+                </Col>
+                <Col>
+                  <img className="imgs float-end" src='assets/img/q.png' alt="logo"/> 
+                </Col>
+              </Row>
+              <Container className='blank'>
               </Container>
-          </Form>
-        </Container>
-      </section>
-      <footer>
-        <Container>
-          <p>FAQ는 이곳으로 하면 됩니다~</p>
-        </Container>
-        <Container>
-          <p>&copy;2023 ReadyBerry</p>
-        </Container>
-      </footer>
-    </div>
+              <Row>
+                <Col>
+                </Col>
+                <Col className='ButtonClass'>
+                  <Button className="btn float-end btnclass"><Nav.Link href="#contact">테스터 신청</Nav.Link></Button>
+                </Col>
+              </Row>
+            </Container>
+          </section>
+        </Slide>
+        <Slide>
+          <section id="into">
+            <Container className='text-center'>
+              <p className='parah'>냠냠플래닛은 기존 오프라인 외식 산업에 컨텐츠 적인 요소를 가미하여 유저에게는 새로운 경험을 제공함과 동시에, 지역 상권 내에 여러 음식점 업주들에게는 더 많은 손님을 견인하여 지역 상권 활성화에도 기여하고자하는 목표를 가지고 있는 서비스 입니다</p>
+              <div className='roulette'>냠냠 룰렛</div>
+              <div className='wheeldiv'>
+                <Wheel
+                  mustStartSpinning={mustSpin}
+                  prizeNumber={prizeNumber}
+                  data={data}
+                  onStopSpinning={() => {
+                    setMustSpin(false);}}/>
+              </div>
+              <Button size="lg" className="btn float-center submitclass" onClick={handleSpinClick} type="submit"> 돌리기!</Button>
+            </Container>
+          </section>
+        </Slide>
+        <Slide>
+          <section id="main">
+            <Container>
+              <h2>냠냠플래닛 사용자 불편사항 이걸로 만들게된 계기</h2>
+              <p className='parah'>이러이러한 목표를 가지고 있습니다</p>
+              <h2>냠냠플래닛 계획</h2>
+              <p>국제관 앞에서 언제 어디서 ~~한 룰렛을 돌릴 예정입니다</p>
+              <h2>냠냠플래닛 베니핏</h2>
+              <p>이러이러한 베니핏이 있을 예정입니다</p>
+            </Container>
+          </section>       
+        </Slide>
+        <Slide>
+          <section id="contact">
+            <Container>
+              <h2>베타테스터 신청</h2>
+                <Form ref={form} onSubmit={sendEmail}>
+                  <Form.Group controlId="formBasicName">
+                    <Form.Label className="formleft" >이름</Form.Label>
+                    <Form.Control type="text" name="name" placeholder="이름 입력" required />
+                  </Form.Group>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>학과</Form.Label>
+                    <Form.Control type="text" name = "department" placeholder="학과 입력" required />
+                  </Form.Group>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>학번</Form.Label>
+                    <Form.Control type="text" name = "studentno" placeholder="학번 입력" required />
+                  </Form.Group>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>이메일</Form.Label>
+                    <Form.Control type="email" name = "email" placeholder="이메일 입력" required />
+                  </Form.Group>
+                  <Container className="text-center">
+                    <Button size="lg" className="btn float-center submitclass" type="submit">
+                      신청하기
+                    </Button>
+                  </Container>
+                </Form>
+            </Container>
+          </section>
+          <footer>
+            <Container>
+              <p>FAQ는 이곳으로 하면 됩니다~</p>
+            </Container>
+            <Container>
+              <p>&copy;2023 ReadyBerry</p>
+            </Container>
+          </footer> 
+            </Slide>
+          </FullPage>
+    </div>  
   );
 }
 
